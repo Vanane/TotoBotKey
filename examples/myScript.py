1b3b5bc0a7bba75030a27199805404bf10e9c83b
Vanane <blackfox248@gmail.com>
2024-12-23 01:06:27 +0100

    Updated example script to give a little more indication and examples

--- examples/myScript.py
+++ examples/myScript.py
@@ -7,32 +7,29 @@ from totoBotKey.commands import type_, click, wait, clickAt
 class MyScript(BaseScript):
     @on("+a")
     @staticmethod
-    def doSmth():
+    def onShiftA():
+        """SExample : macro to type some text when specifically Shift+A is pressed"""
         type_("Shift+A got hit")
 
     @on("a", bType=BindType.ANY)
     @staticmethod
-    def doSmth4():
+    def onAnyA():
+        """Example : macro to type some text whenever A is pressed in any situation"""        
         type_("A got hit")
 
     @on("p", "o")
     @staticmethod
-    def doSmth2():
+    def onAnyPO():
+        """Example : macro binded on several non-mod keys"""
         wait(1000)
         print("One second has passed")
 
     @on("BtnSide", bType=BindType.ANY)
     @staticmethod
-    def doSmth3():        
+    def spamClick():
+        """Example : concrete example of a macro to spamclick the left button, while one of the extra buttons is held down"""
         while inputs.isPressed(Key.BTN_SIDE):
             click(Button.BtnLeft)
-            # wait(15)
-
-    @on("BtnExtra", bType=BindType.ANY)
-    @staticmethod
-    def doSmth5():
-        clickAt(Button.BtnLeft, 400, 400)
-
 
     @staticmethod
     def init():
