ab16da7a1613d7d801ae8e80e4f87111b0289453
Vanane <blackfox248@gmail.com>
2024-12-31 16:17:47 +0100

    Better handling of recording

--- examples/recorder.py
+++ examples/recorder.py
@@ -1,3 +1,8 @@
-from totoBotRec import *
+from TotoBotRec.totoBotRec import *
 
-record(considerCursor=False, considerWait=True, waitThreshold=1000)
+record(considerCursor=False, considerWait=True, waitThreshold=3000)
+
+records = getRecords()
+print(records[0])
+for r in records[1:]:
+    print(f"Then {str(r)}")
